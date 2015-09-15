77dd271d6fe217670918aa261047405d8b4b6755
Joris Roovers <jroovers@cisco.com>
2015-09-15 10:29:55 +0200

    Determine files changed in last commit
    When no commit message is passed to stdin, gitlint currently determines
    the commit message to lint by taking the commit message of the directory
    it is currently in. This commit adds support for gitlint to also fetch
    the changed files of the last commit. This will be used to create more
    complex rules.
    
    Also: added wheel requirement so that we can do distribute a wheel
    alongside the source distribution.

--- gitlint/git.py
+++ gitlint/git.py
@@ -10,4 +10,7 @@ class GitCommitInfo(object):
     def get_latest_commit():
         commit_info = GitCommitInfo()
         commit_info.commit_msg = sh.git.log("-1", "--pretty=%B", _tty_out=False)
+        # changed files in last commit
+        changed_files_str = sh.git("diff-tree", "--no-commit-id", "--name-only", "-r", "HEAD", _tty_out=False)
+        commit_info.changed_files = [changed_file for changed_file in changed_files_str.strip().split("\n")]
         return commit_info
