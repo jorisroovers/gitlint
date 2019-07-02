ce8b4731a0622b762a07d0f6840402a9900b8361
Joris Roovers <joris.roovers@gmail.com>
2019-07-02 12:32:04 +0200

    Added create-test-repo.sh script
    This script allows developers to easily create test repos.

---
+++ tools/create-test-repo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+RED="\033[31m"
+YELLOW="\033[33m"
+BLUE="\033[94m"
+GREEN="\033[32m"
+NO_COLOR="\033[0m"
+
+CWD="$(pwd)"
+echo "pwd=$CWD"
+# Create the repo
+cd /tmp
+reponame=$(date +gitlint-test-%Y-%m-%d_%H-%M-%S)
+git init $reponame
+cd $reponame
+
+# Do some basic config
+git config user.name gitlint-test-user
+git config user.email gitlint@test.com
+git config core.quotePath false
+git config core.precomposeunicde true
+
+# Add a test commit
+echo "test 123" > test.txt
+git add test.txt
+git commit -m "test commit title\n\ntest commit body that has a bit more text\n"
+cd $CWD
+
+# Let the user know
+echo ""
+echo -e "Created $GREEN/tmp/${reponame}$NO_COLOR"
+echo "Hit key up to access 'cd /tmp/$reponame'"
+echo "(Run this script using 'source' for this to work)"
+history -s "cd /tmp/$reponame"
