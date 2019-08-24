3c629efecee828d2f3027fc76bb7c5516a30540c
Joris Roovers <joris.roovers@gmail.com>
2019-08-24 15:38:33 +0200

    Improved encoding detection on Windows
    Gitlint will now respect the LC_ALL, LC_CTYPE and LANG environment variables
    on Windows - in line with git's behavior.
    
    The default fallback is UTF-8, this should lead to improved unicode
    parsing for the majority of users.
    
    This is a partial fix for #96.
    
    Also includes a minor refactoring of utils.py that makes it easier to unit test
    some of the constants that are defined in that module.

---
+++ gitlint/tests/test_utils.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+from gitlint import utils
+from gitlint.tests.base import BaseTestCase
+
+try:
+    # python 2.x
+    from mock import patch
+except ImportError:
+    # python 3.x
+    from unittest.mock import patch  # pylint: disable=no-name-in-module, import-error
+
+
+class UtilsTests(BaseTestCase):
+
+    def tearDown(self):
+        # Since we're messing around with `utils.PLATFORM_IS_WINDOWS` during these tests, we need to reset
+        # its value after we're done this doesn't influence other tests
+        utils.PLATFORM_IS_WINDOWS = utils.platform_is_windows()
+
+    @patch('os.environ')
+    def test_use_sh_library(self, patched_env):
+        patched_env.get.return_value = "1"
+        self.assertEqual(utils.use_sh_library(), True)
+        patched_env.get.assert_called_once_with("GITLINT_USE_SH_LIB", None)
+
+        for invalid_val in ["0", u"foöbar"]:
+            patched_env.get.reset_mock()  # reset mock call count
+            patched_env.get.return_value = invalid_val
+            self.assertEqual(utils.use_sh_library(), False, invalid_val)
+            patched_env.get.assert_called_once_with("GITLINT_USE_SH_LIB", None)
+
+        # Assert that when GITLINT_USE_SH_LIB is not set, we fallback to checking whether we're on Windows
+        utils.PLATFORM_IS_WINDOWS = True
+        patched_env.get.return_value = None
+        self.assertEqual(utils.use_sh_library(), False)
+
+        utils.PLATFORM_IS_WINDOWS = False
+        self.assertEqual(utils.use_sh_library(), True)
+
+    @patch('gitlint.utils.locale')
+    def test_default_encoding_non_windows(self, mocked_locale):
+        utils.PLATFORM_IS_WINDOWS = False
+        mocked_locale.getpreferredencoding.return_value = u"foöbar"
+        self.assertEqual(utils.getpreferredencoding(), u"foöbar")
+        mocked_locale.getpreferredencoding.assert_called_once()
+
+        mocked_locale.getpreferredencoding.return_value = False
+        self.assertEqual(utils.getpreferredencoding(), u"UTF-8")
+
+    @patch('os.environ')
+    def test_default_encoding_windows(self, patched_env):
+        utils.PLATFORM_IS_WINDOWS = True
+        # Mock out os.environ
+        mock_env = {}
+
+        def mocked_get(key, default):
+            return mock_env.get(key, default)
+
+        patched_env.get.side_effect = mocked_get
+
+        # Assert getpreferredencoding reads env vars in order: LC_ALL, LC_CTYPE, LANG
+        mock_env = {"LC_ALL": u"lc_all_välue", "LC_CTYPE": u"foo", "LANG": u"bar"}
+        self.assertEqual(utils.getpreferredencoding(), u"lc_all_välue")
+        mock_env = {"LC_CTYPE": u"lc_ctype_välue", "LANG": u"hur"}
+        self.assertEqual(utils.getpreferredencoding(), u"lc_ctype_välue")
+        mock_env = {"LANG": u"lang_välue"}
+        self.assertEqual(utils.getpreferredencoding(), u"lang_välue")
+
+        # Assert split on dot
+        mock_env = {"LANG": u"foo.bär"}
+        self.assertEqual(utils.getpreferredencoding(), u"bär")
+
+        # assert default encoding is UTF-8
+        mock_env = {}
+        self.assertEqual(utils.getpreferredencoding(), "UTF-8")
+        mock_env = {"FOO": u"föo"}
+        self.assertEqual(utils.getpreferredencoding(), "UTF-8")
--- gitlint/utils.py
+++ gitlint/utils.py
@@ -3,19 +3,75 @@ import platform
 import sys
 import os
 
-from locale import getpreferredencoding
+import locale
 
-DEFAULT_ENCODING = getpreferredencoding() or "UTF-8"
+# Note: While we can easily inline the logic related to the constants set in this module, we deliberately create
+# small functions that encapsulate that logic as this enables easy unit testing. In particular, by creating functions
+# we can easily mock the dependencies during testing, which is not possible if the code is not enclosed in a function
+# and just executed at import-time.
+
+########################################################################################################################
 LOG_FORMAT = '%(levelname)s: %(name)s %(message)s'
 
+########################################################################################################################
+# PLATFORM_IS_WINDOWS
+
+
+def platform_is_windows():
+    return "windows" in platform.system().lower()
+
+
+PLATFORM_IS_WINDOWS = platform_is_windows()
+
+########################################################################################################################
+# USE_SH_LIB
+# Determine whether to use the `sh` library
 # On windows we won't want to use the sh library since it's not supported - instead we'll use our own shell module.
 # However, we want to be able to overwrite this behavior for testing using the GITLINT_USE_SH_LIB env var.
-PLATFORM_IS_WINDOWS = "windows" in platform.system().lower()
-GITLINT_USE_SH_LIB_ENV = os.environ.get('GITLINT_USE_SH_LIB', None)
-if GITLINT_USE_SH_LIB_ENV:
-    USE_SH_LIB = (GITLINT_USE_SH_LIB_ENV == "1")
-else:
-    USE_SH_LIB = not PLATFORM_IS_WINDOWS
+
+
+def use_sh_library():
+    gitlint_use_sh_lib_env = os.environ.get('GITLINT_USE_SH_LIB', None)
+    if gitlint_use_sh_lib_env:
+        return gitlint_use_sh_lib_env == "1"
+    return not PLATFORM_IS_WINDOWS
+
+
+USE_SH_LIB = use_sh_library()
+
+########################################################################################################################
+# DEFAULT_ENCODING
+
+
+def getpreferredencoding():
+    """ Modified version of local.getpreferredencoding() that takes into account LC_ALL, LC_CTYPE, LANG env vars
+        on windows and falls back to UTF-8. """
+    default_encoding = locale.getpreferredencoding() or "UTF-8"
+
+    # On Windows, we mimic git/linux by trying to read the LC_ALL, LC_CTYPE, LANG env vars manually
+    # (on Linux/MacOS the `getpreferredencoding()` call will take care of this).
+    # We fallback to UTF-8
+    if PLATFORM_IS_WINDOWS:
+        default_encoding = "UTF-8"
+        for env_var in ["LC_ALL", "LC_CTYPE", "LANG"]:
+            encoding = os.environ.get(env_var, False)
+            if encoding:
+                # Support dotted (C.UTF-8) and non-dotted (C or UTF-8) charsets:
+                # If encoding contains a dot: split and use second part, otherwise use everything
+                dot_index = encoding.find(".")
+                if dot_index != -1:
+                    default_encoding = encoding[dot_index + 1:]
+                else:
+                    default_encoding = encoding
+                break
+
+    return default_encoding
+
+
+DEFAULT_ENCODING = getpreferredencoding()
+
+########################################################################################################################
+# Unicode utility functions
 
 
 def ustr(obj):
