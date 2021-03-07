ce65043a6fc65808c7a70a9535b11bb045d2d913
Joris Roovers <joris.roovers@gmail.com>
2021-03-07 12:24:38 +0100

    Various dependency upgrades
    wheel:            0.35.1 -> 0.36.2
    arrow:            0.17.0 -> 1.0.2
    
    Test dependencies:
    pytest:           6.1.2  -> 6.2.2
    coverage:         5.3    -> 5.5
    radon:            4.3.2  -> 4.4.0
    pylint:           2.6.0  -> 2.7.2
    python-coveralls: 2.9.2  -> 2.9.3

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     python_requires=">=3.6",
     install_requires=[
         'Click==7.1.2',
-        'arrow==0.17.0',
+        'arrow==1.0.2',
     ],
     extras_require={
         ':sys_platform != "win32"': [
