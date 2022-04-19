a0134c835a68109a742598ea4f1c93aa6d37bcc7
Qiangning Hong <hongqn@yashihq.com>
2022-04-20 05:12:17 +0800

    fix: Not fail when current branch has no commits. (#214)
    fixes #189.
    
    It uses `git branch --show-current` to get current branch when the old
    way (`git rev-parse --abbrev-ref HEAD`) fails.
    
    Co-authored-by: Joris Roovers <joris.roovers@gmail.com>

--- gitlint-core/gitlint/tests/git/test_git.py
+++ gitlint-core/gitlint/tests/git/test_git.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 from gitlint.shell import ErrorReturnCode, CommandNotFound
 
@@ -64,8 +64,12 @@ class GitTests(BaseTestCase):
 
         # assert that commit message was read using git command
         sh.git.assert_called_once_with("log", "-1", "--pretty=%H", **self.expected_sh_special_args)
-        sh.git.reset_mock()
 
+    @patch('gitlint.git.sh')
+    def test_git_no_commits_get_branch(self, sh):
+        """ Check that we can still read the current branch name when there's no commits. This is useful when
+            when trying to lint the first commit using the --staged flag.
+        """
         # Unknown reference 'HEAD' commits: returned by 'git rev-parse'
         err = (b"HEAD"
                b"fatal: ambiguous argument 'HEAD': unknown revision or path not in the working tree."
@@ -74,15 +78,22 @@ class GitTests(BaseTestCase):
 
         sh.git.side_effect = [
             "#\n",  # git config --get core.commentchar
-            ErrorReturnCode("rev-parse --abbrev-ref HEAD", b"", err)
+            ErrorReturnCode("rev-parse --abbrev-ref HEAD", b"", err),
+            'test-branch',  # git branch --show-current
         ]
 
-        with self.assertRaisesMessage(GitContextError, expected_msg):
-            context = GitContext.from_commit_msg("test")
-            context.current_branch
+        context = GitContext.from_commit_msg("test")
+        self.assertEqual(context.current_branch, 'test-branch')
 
-        # assert that commit message was read using git command
-        sh.git.assert_called_with("rev-parse", "--abbrev-ref", "HEAD", _tty_out=False, _cwd=None)
+        # assert that we try using `git rev-parse` first, and if that fails (as will be the case with the first commit),
+        #  we fallback to `git branch --show-current` to determine the current branch name.
+        expected_calls = [
+            call("config", "--get", "core.commentchar", _tty_out=False, _cwd=None, _ok_code=[0, 1]),
+            call("rev-parse", "--abbrev-ref", "HEAD", _tty_out=False, _cwd=None),
+            call("branch", "--show-current", _tty_out=False, _cwd=None)
+        ]
+
+        self.assertEqual(sh.git.mock_calls, expected_calls)
 
     @patch("gitlint.git._git")
     def test_git_commentchar(self, git):
