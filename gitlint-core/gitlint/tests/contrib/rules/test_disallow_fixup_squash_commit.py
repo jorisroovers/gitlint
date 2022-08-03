b8541a5d899e677e2af1b007726e35fa8a732c86
Matthias Beyer <mail@beyermatthias.de>
2022-08-03 08:12:24 +0200

    Add test for disallow_fixup_squash_commit rule
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ gitlint-core/gitlint/tests/contrib/rules/test_disallow_fixup_squash_commit.py
@@ -0,0 +1,34 @@
+
+# -*- coding: utf-8 -*-
+from gitlint.tests.base import BaseTestCase
+from gitlint.rules import RuleViolation
+from gitlint.contrib.rules.signedoff_by import SignedOffBy
+
+from gitlint.config import LintConfig
+
+
+class ContribSignedOffByTests(BaseTestCase):
+
+    def test_enable(self):
+        # Test that rule can be enabled in config
+        for rule_ref in ['CC2', 'contrib-disallow-fixup-squash']:
+            config = LintConfig()
+            config.contrib = [rule_ref]
+            self.assertIn(DisallowFixupSquashCommit(), config.rules)
+
+    def test_signedoff_by(self):
+        # No violations when no 'fixup!' line and no 'squash!' line is present
+        rule = DisallowFixupSquashCommit()
+        violations = rule.validate(self.gitcommit("Föobar\n\nMy Body"))
+        self.assertListEqual([], violations)
+
+        # Assert violation when 'fixup!' in title
+        violations = rule.validate(self.gitcommit("fixup! Föobar\n\nMy Body"))
+        expected_violation = RuleViolation("CC1", "Fixup commits are not allowed", line_nr=1)
+        self.assertListEqual(violations, [expected_violation])
+
+        # Assert violation when 'squash!' in title
+        violations = rule.validate(self.gitcommit("!squash Föobar\n\nMy Body"))
+        expected_violation = RuleViolation("CC1", "Squash commits are not allowed", line_nr=1)
+        self.assertListEqual(violations, [expected_violation])
+
