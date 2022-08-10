135806020a0bc7fe53592cc5eeb5cb484e91f15d
Matthias Beyer <mail@beyermatthias.de>
2022-08-10 10:44:49 +0200

    Fix filename
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ gitlint-core/gitlint/contrib/rules/disallow_cleanup_commits.py
@@ -0,0 +1,20 @@
+from gitlint.rules import CommitRule, RuleViolation
+
+class DisallowCleanupCommits(CommitRule):
+    """ This rule checks the commits for "fixup!"/"squash!"/"amend!" commits
+    and rejects them.
+    """
+
+    name = "contrib-disallow-cleanup-commits"
+    id = "CC2"
+
+    def validate(self, commit):
+        if commit.is_fixup_commit:
+            return [RuleViolation(self.id, "Fixup commits are not allowed", line_nr=1)]
+
+        if commit.is_squash_commit:
+            return [RuleViolation(self.id, "Squash commits are not allowed", line_nr=1)]
+
+        if commit.message.title.startswith("amend!"):
+            return [RuleViolation(self.id, "Amend commits are not allowed", line_nr=1)]
+
