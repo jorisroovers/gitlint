840ebabc51f537a8bbdeaa0e06cf9b39de8b36cf
Joris Roovers <joris.roovers@gmail.com>
2022-11-08 10:57:06 +0100

    Devcontainer support (#361)
    Support for devcontainers for easy development using VSCode.

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -x
+
+brew install asdf
+source "$(brew --prefix asdf)/libexec/asdf.sh"
+
+# Install latest python
+asdf plugin add python
+asdf install python 3.11.0
+asdf global python 3.11.0
+
+# You can easily install other python versions like so:
+# asdf install python 3.6.15
+# asdf install python 3.7.15
+# asdf install python 3.8.15
+# asdf install python 3.9.15
+# asdf install python 3.10.8
+# asdf install python pypy3.9-7.3.9
+
+# Setup virtualenv, install all dependencies
+cd /workspaces/gitlint
+$(asdf which python) -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt -r test-requirements.txt -r doc-requirements.txt
