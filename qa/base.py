41af46727d42fc0ce61a975a37544c31acc09ad2
Joris Roovers <joris.roovers@gmail.com>
2022-12-15 10:56:18 +0000

    fixup! Expected files are always UTF-8

--- qa/base.py
+++ qa/base.py
@@ -182,6 +182,7 @@ class BaseTestCase(TestCase):
         specified by variable_dict."""
         expected_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "expected")
         expected_path = os.path.join(expected_dir, filename)
+        #  Expected files are UTF-8 encoded (not dependent on the system's default encoding)
         with open(expected_path, encoding="UTF-8") as file:
             expected = file.read()
 
