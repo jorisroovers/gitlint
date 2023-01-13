dabe285f69226367a5a909c1e3647023d7f1d670
Joris Roovers <joris.roovers@gmail.com>
2023-01-13 09:57:26 +0000

    Custom hatch_build.py hook
    Use to ensure that gitlint and gitlint-core[trusted-deps] versions always match

--- gitlint-core/gitlint/__init__.py
+++ gitlint-core/gitlint/__init__.py
@@ -1,1 +1,3 @@
-__version__ = "0.19.0.dev3"
+from importlib.metadata import version
+
+__version__ = version("gitlint-core")
---
+++ hatch_build.py
@@ -0,0 +1,8 @@
+from hatchling.metadata.plugin.interface import MetadataHookInterface
+
+
+class CustomMetadataHook(MetadataHookInterface):
+    """Custom metadata hook for hatch that ensures that gitlint and gitlint-core[trusted-deps] versions always match"""
+
+    def update(self, metadata: dict) -> None:
+        metadata["dependencies"] = [f"gitlint-core[trusted-deps]=={metadata['version']}"]
