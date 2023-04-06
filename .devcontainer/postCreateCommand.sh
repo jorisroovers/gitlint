c54c5be1257394fc6142079c21861a51826b98a9
Joris Roovers <joris.roovers@gmail.com>
2023-04-06 11:33:16 +0000

    Docs: info on testing different python versions
    The docs were missing important steps on how to test gitlint against
    different python versions on the dev container.
    
    Also fixes the Devcontainer's postCreateCommand.sh to install hatch
    using pip instead of homebrew.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -x
 
 brew install asdf
-brew install hatch
 source "$(brew --prefix asdf)/libexec/asdf.sh"
 
 # Install latest python
@@ -10,15 +9,17 @@ asdf install python 3.11.0
 asdf global python 3.11.0
 
 # You can easily install other python versions like so:
-# asdf install python 3.6.15
 # asdf install python 3.7.15
 # asdf install python 3.8.15
 # asdf install python 3.9.15
 # asdf install python 3.10.8
 # asdf install python pypy3.9-7.3.9
 
+# If you do this, you also need to install hatch for each python version
+# asdf global python 3.7.15
+# pip install hatch==1.6.3
+
 # Setup virtualenv, install all dependencies
 cd /workspaces/gitlint
-$(asdf which python) -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt -r test-requirements.txt -r doc-requirements.txt
+pip install hatch==1.6.3
+hatch env create
