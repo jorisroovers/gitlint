5731d2d8e583c6b15dcf27831cb16651c150b42b
Joris Roovers <joris.roovers@gmail.com>
2023-05-10 08:48:40 +0000

    Deployed 149774c with MkDocs version: 1.4.3

--- javascript/extra.js
+++ javascript/extra.js
@@ -3,11 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
         new Termynal(termynalEl);
     });
 
-    // Redirect /gitlint/configuration/#regex-style-search to /gitlint/configuration/general_options/#regex-style-search
+    // Redirect /gitlint/configuration/#<option> to /gitlint/configuration/general_options/#<option>
     // This is to support old links that are in gitlint's CLI Output
     // If the trailing slash is missing from /gitlint/configuration, mkdocs will redirect to the trailing slash version,
     // and then this code will redirect to the correct page
-    if (window.location.pathname == "/gitlint/configuration/" && window.location.hash === "#regex-style-search") {
-        window.location.href = "/gitlint/configuration/general_options/#regex-style-search";
+    if (window.location.pathname == "/gitlint/configuration/") {
+        if (window.location.hash === "#regex-style-search") {
+            window.location.href = "/gitlint/configuration/general_options/#regex-style-search";
+        } else if (window.location.hash === "#staged") {
+            window.location.href = "/gitlint/configuration/general_options/#staged";
+        }
     }
 });
